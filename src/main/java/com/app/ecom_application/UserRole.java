02e87f05d25cac23cdd7d18d81f13be88112aec1
Chetan <ck95263@gmail.com>
2026-05-24 02:32:31 +0530

    Add H2 database configuration, JPA integration, and user role enum

---
+++ src/main/java/com/app/ecom_application/UserRole.java
@@ -0,0 +1,5 @@
+package com.app.ecom_application;
+
+public enum UserRole {
+    CUSTOMER , ADMIN
+}
--- src/main/java/com/app/ecom_application/UserService.java
+++ src/main/java/com/app/ecom_application/UserService.java
@@ -1,36 +1,31 @@
 package com.app.ecom_application;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private final List<User> userList = new ArrayList<>();
-    private Long currentId = 1L;
+    private final UserRepo userRepo;
+
     public Optional<User> fetchUser(Long id){
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
+        return userRepo.findById(id);
     }
     public void addUser(User user) {
-        user.setId(currentId++);
-        userList.add(user);
+        userRepo.save(user);
     }
     public List<User> fetchAllUser(){
-        return userList;
+        return userRepo.findAll();
     }
     public boolean updateUser(Long id , User updatedUser) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
-                .map(existedUser -> {
-                    existedUser.setFirstName(updatedUser.getFirstName());
-                    existedUser.setLastName(updatedUser.getLastName());
-                    return true;
-                }).orElse(false);
+        return userRepo.findById(id).map(existingUser -> {
+            existingUser.setFirstName(updatedUser.getFirstName());
+            existingUser.setLastName(updatedUser.getFirstName());
+            return true;
+        }).orElse(false);
     }
 
 }
