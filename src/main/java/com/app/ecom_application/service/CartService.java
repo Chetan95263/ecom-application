7dbfdc5b0f4ef5e4ce396281f47f4891a00d14c3
Chetan <ck95263@gmail.com>
2026-05-28 00:57:10 +0530

    add addToCart , deleteFromCart endpoints

---
+++ src/main/java/com/app/ecom_application/service/CartService.java
@@ -0,0 +1,66 @@
+package com.app.ecom_application.service;
+
+import com.app.ecom_application.dto.CartItemRequest;
+import com.app.ecom_application.model.CartItem;
+import com.app.ecom_application.model.Product;
+import com.app.ecom_application.model.User;
+import com.app.ecom_application.repository.CartItemRepo;
+import com.app.ecom_application.repository.ProductRepo;
+import com.app.ecom_application.repository.UserRepo;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+    private final ProductRepo productRepo;
+    private final UserRepo userRepo;
+    private final CartItemRepo cartItemRepo;
+    public boolean addToCart(String userId, CartItemRequest request) {
+        // Look for product
+        Optional<Product> productOpt = productRepo.findById(request.getProductId());
+        if(productOpt.isEmpty())
+            return false;
+        Product product = productOpt.get();
+        if(product.getStockQuantity() < request.getQuantity())
+            return false;
+        Optional<User> userOpt = userRepo.findById(Long.valueOf(userId));
+        if(userOpt.isEmpty())
+            return false;
+        User user = userOpt.get();
+        CartItem existingCartItem = cartItemRepo.findByUserAndProduct(user , product);
+        if(existingCartItem != null) {
+            // Update the quantity
+            existingCartItem.setQuantity(existingCartItem.getQuantity()+ request.getQuantity());
+            existingCartItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(existingCartItem.getQuantity())));
+            cartItemRepo.save(existingCartItem);
+        }
+        else {
+            // Create new cart item
+            CartItem cartItem = new CartItem();
+            cartItem.setUser(user);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(request.getQuantity());
+            cartItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+
+        }
+        return true;
+    }
+
+    public boolean deleteItemFromCart(String userId, Long productId) {
+        Optional<Product> productOpt = productRepo.findById(productId);
+        Optional<User> userOpt = userRepo.findById(Long.valueOf(userId));
+
+        if(productOpt.isPresent() && userOpt.isPresent()) {
+            cartItemRepo.deleteByUserAndProduct(userOpt.get() , productOpt.get());
+            return true;
+        }
+        return false;
+    }
+
+}
